0420d06fe2195ae1203f75707d751c1a1a98598f
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2025-05-23 15:57:04 +0200

    feat => login endpoint

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -10,6 +10,6 @@ router.post('/create', controller.create.bind(controller));
 router.get('/get-user/:id', controller.getOne.bind(controller));
 router.post('/delete-user', controller.delete.bind(controller));
 router.post('/modify', controller.modify.bind(controller));
-
+router.post('/login', controller.login.bind(controller));
 
 export default router;
