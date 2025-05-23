e9968bb3a592a5633fb0c39e899c63c1b8353fec
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2025-05-23 15:32:17 +0200

    feat => cors

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
-import express from 'express';
-import commentRoutes from './routes/commentRoutes';
-import postRoutes from './routes/postRoutes';
-import userRoutes from './routes/userRoutes';
+import cors from "cors";
+import express from "express";
+import commentRoutes from "./routes/commentRoutes";
+import postRoutes from "./routes/postRoutes";
+import userRoutes from "./routes/userRoutes";
 
 const app = express();
 
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
-app.use('/api/posts', postRoutes);
-app.use('/api/comment', commentRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comment", commentRoutes);
 
 export default app;
